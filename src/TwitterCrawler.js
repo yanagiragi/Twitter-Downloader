2cecece08fb3b18a79184afc65a3812c79693fc0
yanagiragi <qaz90473@gmail.com>
2019-05-13 23:51:20 +0800

    add img src to outputs

--- src/TwitterCrawler.js
+++ src/TwitterCrawler.js
@@ -1,6 +1,16 @@
 const requestPromise = require('request-promise')
 const cheerio = require('cheerio')
 
+class TwitterTweet
+{
+    constructor(tweetId, photos, timestamp){
+        this.tweetId = tweetId
+        this.photos = photos
+        this.timestamp = timestamp
+        this.hasPhoto = photos.length > 0
+    }
+}
+
 class TwitterCrawler
 {
     constructor(account, startDate, endDate) {
@@ -8,41 +18,55 @@ class TwitterCrawler
         this.startDate = startDate
         this.endDate = endDate
         this.fetchResults = []
+        this.lang = 'en-us'
     }
 
-    Fetch(position, depth=0){
+    Fetch(position, depth=0) {
         // When depth is 0, which is our first fetch
         // Use min_position to get the lastest 5 tweets
         // Else, use max_position to get next 20 tweets
-        let url = `https://twitter.com/i/search/timeline?vertical=default&q=from%3A${this.account}%20since%3A${this.startDate}%20until%3A${this.endDate}&src=typd&include_available_features=1&include_entities=1&lang=zh-tw&${depth == 0 ? 'min' : 'max'}_position=${position}`
+        let url = `https://twitter.com/i/search/timeline?vertical=default&q=from%3A${this.account}%20since%3A${this.startDate}%20until%3A${this.endDate}&src=typd&include_available_features=1&include_entities=1&lang=${this.lang}}&${depth == 0 ? 'min' : 'max'}_position=${position}`
         return requestPromise(url)
     }
 
     Parse(htmlString) {
         let data = JSON.parse(htmlString)    
         let position = data["max_position"];    
-        if(!position){
+        if(!position) {
             position = data["min_position"]
         }
         
-        let $ = cheerio.load(data['items_html'].replace(/\\\"/g,/\"/))    
-        let timestamps = $('.tweet-timestamp')
-        
-        let ids = []
-        for(let i = 0; i < timestamps.length; ++i){
-            ids.push(timestamps[i].attribs.href)
-        }
+        let $ = cheerio.load(data['items_html'].replace(/\\\"/g,/\"/))
+        let raw = $('.stream-item')
 
-        // format '/hmw59750476/status/1116955486270545920' to '1116955486270545920'
-        ids = ids.map(x => x.substring(x.lastIndexOf('/')+1))
+        let container = []
+
+        for(let i = 0; i < raw.length; ++i) {
+            let data = raw[i]
+            
+            // format '/hmw59750476/status/1116955486270545920' to '1116955486270545920'
+            let tweetId = $('.tweet-timestamp', data)[0].attribs.href
+            tweetId = tweetId.substring(tweetId.lastIndexOf('/')+1)
+            
+            let timestamp = $('.tweet-timestamp', data)[0].attribs.title
+            
+            let rawPhotos = $('.js-adaptive-photo', data)
+            let photos = []
+            for(let j = 0; j < rawPhotos.length; ++j){
+                photos.push(rawPhotos[j].attribs['data-image-url'])
+            }
+            
+            container.push(new TwitterTweet(tweetId, photos, timestamp))
+        }
 
         // return nextPosition, resultIds, hasNext
         // remember to use trim(), since no data means data[items_html] is '\n\n\n\n\n\n\n \n'
-        return [position, ids, data['items_html'].trim().length != 0]
+        return [position, container, data['items_html'].trim().length != 0]
     }
     
     // default position means nothing, just a placeholder
-    async Crawl (position='Haku_Is_Waifu', depth=0, maxDepth=2) {    
+    // maxDepth < 0 means search until there is no data
+    async Crawl (position='Haku_Is_Waifu', depth=0, maxDepth=-1) {    
         const requestResult = await this.Fetch(position, depth)
         const [nextPosition, resultIds, hasNext] = this.Parse(requestResult)
 
@@ -50,9 +74,7 @@ class TwitterCrawler
             this.fetchResults.push(element)
         });
 
-        // console.log([depth, nextPosition, hasNext, resultIds])
-        if (hasNext && depth <= maxDepth){
-            console.log('depth = ', depth)
+        if (hasNext && maxDepth > 0 && depth <= maxDepth){
             return this.Crawl(nextPosition, depth + 1)
         }
         else {
@@ -71,4 +93,5 @@ if (require.main === module) {
     })
 }
 
+exports.TwitterTweet = TwitterTweet
 exports.TwitterCrawler = TwitterCrawler
