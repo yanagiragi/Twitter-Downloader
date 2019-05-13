731c57e7dc2088c24d12ff18875f165dc85718f1
yanagiragi <qaz90473@gmail.com>
2019-05-14 03:53:51 +0800

    update image download feature

--- bin/cli.js
+++ bin/cli.js
@@ -1,48 +1,23 @@
 #!/usr/bin/env node
 const fs = require('fs-extra')
 const minimist = require('minimist')
-const { TwitterTweet, TwitterCrawler } = require('..')
-const { DateFormat, IncreaseDate } = require('./utils')
+const { TwitterCrawler } = require('..')
+const { DateFormat, IncreaseDate, FetchImage } = require('./utils')
 
 const daySkip = 7
+const saveDuration = 50
+
+const args = minimist(process.argv.slice(2));
+
+const mode = args.mode || "info"
 
 const StoragePath = __dirname + '/Storage'
 const dataPath = __dirname + '/data/data.json'
 const containerPath = __dirname + '/data/container.json'
 const currentDate = DateFormat(new Date())
 
-if (require.main === module) {
-
-    fs.ensureDirSync(StoragePath)
-
-    let data = []
-    if (!fs.existsSync(dataPath)){
-        console.log(`${dataPath} does not exists. Abort.`)
-        process.exit()
-    }
-    else {
-        let rawData = fs.readFileSync(dataPath)
-        try {
-            data = JSON.parse(rawData)
-        }
-        catch(err) {
-            console.log(`Failed Parsing ${dataPath}, error = ${err}}`)
-            process.exit()
-        }
-    }
-
-    let containers = {}
-    if(fs.existsSync(containerPath)){
-        let rawContainer = fs.readFileSync(containerPath)
-        try {
-            containers = JSON.parse(rawContainer)
-        }
-        catch(err) {
-            console.log(`Failed Parsing ${containerPath}, error = ${err}}`)
-            process.exit()
-        }
-    }
-
+function UpdateInfo()
+{
     Promise.all(
         data.map (async user => {
             let account = user.id
@@ -71,7 +46,7 @@ if (require.main === module) {
                         }
                     })
 
-                    if(updateCount > 50 || updateCount % 50 == 0){
+                    if(updateCount > saveDuration){
                         fs.writeFileSync(dataPath, JSON.stringify(data, null, 4))
                         fs.writeFileSync(containerPath, JSON.stringify(containers, null, 4))
                         updateCount = 0
@@ -89,4 +64,78 @@ if (require.main === module) {
         fs.writeFileSync(containerPath, JSON.stringify(containers, null, 4))
         console.log(`Done.`)
     })
+}
+
+function UpdateImage()
+{
+    Promise.all(
+        data.map (async user => {
+            fs.ensureDirSync(`${StoragePath}/${user.id}`)
+
+            const img = containers[user.id].reduce((acc, ele) => {
+                if (ele.hasPhoto)
+                    return acc.concat([...ele.photos])
+                else
+                    return acc
+            }, [])
+
+            img.map(async x => {
+                let isDownload = await FetchImage(x, `${StoragePath}/${user.id}/${x.substring(x.lastIndexOf('/')+1)}`)
+                if (isDownload)
+                    console.log(`Successfully Download ${x}.`)
+            })
+        })
+    )
+}
+
+if (require.main === module) {
+
+    fs.ensureDirSync(StoragePath)
+
+    var data = []
+    if (!fs.existsSync(dataPath)){
+        console.log(`${dataPath} does not exists. Abort.`)
+        process.exit()
+    }
+    else {
+        let rawData = fs.readFileSync(dataPath)
+        try {
+            data = JSON.parse(rawData)
+        }
+        catch(err) {
+            console.log(`Failed Parsing ${dataPath}, error = ${err}}`)
+            process.exit()
+        }
+    }
+
+    var containers = {}
+    if(fs.existsSync(containerPath)){
+        let rawContainer = fs.readFileSync(containerPath)
+        try {
+            containers = JSON.parse(rawContainer)
+        }
+        catch(err) {
+            console.log(`Failed Parsing ${containerPath}, error = ${err}}`)
+            process.exit()
+        }
+    }
+
+    if (mode == "info"){
+        console.log("============================================")
+        console.log("                UPDATE INFO")
+        console.log("============================================")
+        UpdateInfo()
+    }
+    else if(mode == "image"){
+        console.log("============================================")
+        console.log("                UPDATE IMAGE")
+        console.log("============================================")
+        UpdateImage()
+    }
+    else {
+        console.log("Error When Parsing Arguments.")
+        console.log("Abort.")
+        process.exit()
+    }
+    
 }
--- bin/utils.js
+++ bin/utils.js
@@ -1,3 +1,6 @@
+const rp = require('request-promise')
+const fs = require('fs-extra')
+
 function DateFormat(date) {
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
 }
@@ -13,6 +16,25 @@ function IncreaseDate(dateString, dayCount=1){
     return DateFormat(date)
 }
 
+async function FetchImage(url, filename) {
+    if (fs.existsSync(filename)){
+        return false
+    }
+    else {
+        try{
+            let result = await rp({ url: url, encoding: 'binary'})
+            fs.writeFileSync(filename, result, 'binary')
+        }
+        catch(error){
+            console.log(`Error when download ${url}`)
+            return false
+        }
+        
+        return true
+    }
+}
+
 exports.DateFormat = DateFormat
 exports.FormatDate = FormatDate
 exports.IncreaseDate = IncreaseDate
+exports.FetchImage = FetchImage
